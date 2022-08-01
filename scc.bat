480c5458580c62e8a9486feb505c0ea6a20a7751
Skyler Burwell <skyboy49707@gmail.com>
2022-07-31 20:54:03 -0700

    Update scc script.

--- scc.bat
+++ scc.bat
@@ -1,2 +1,2 @@
 @echo off
-cloc . --by-file --exclude-dir=lib,cmake-build-debug,cmake-build-release,.idea,.git,docs --not-match-f="CMakeLists.txt|scc" --quiet --report-file=scc.txt
+cloc . --by-file --exclude-dir=lib,cmake-build-debug,cmake-build-release,.idea,.git,docs,benchmark --not-match-f="CMakeLists.txt|scc" --quiet --report-file=scc.txt
