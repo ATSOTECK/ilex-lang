8fb5b22349dc5c897c58eeaeaf17ab3dbcf17f62
Skyler Burwell <skyboy49707@gmail.com>
2023-11-20 20:40:46 -0500

    Add help text.

--- c/main.c
+++ c/main.c
@@ -4,13 +4,24 @@
 
 #include "ilex.h"
 
+void printHelp() {
+    printf("version -v ------ Print version number\n");
+    printf("info    -i ------ Print build info\n");
+    printf("help    -h ------ Print help text\n");
+    printf("\n");
+    printf("Usage: ilex [path]\n");
+}
+
 int main(int argc, char **argv) {
     if (argc >= 2) {
-        if (strcmp("-v", argv[1]) == 0) {
+        if (strcmp("-v", argv[1]) == 0 || strcmp("version", argv[1]) == 0) {
             printf("%s\n", ILEX_VERSION);
             return 0;
-        } else if (strcmp("-V", argv[1]) == 0) {
+        } else if (strcmp("-i", argv[1]) == 0 || strcmp("info", argv[1]) == 0) {
             printf("Ilex version %s built on %s.\n", ILEX_VERSION, ILEX_DATE);
+            return 0;
+        } else if (strcmp("-h", argv[1]) == 0 || strcmp("help", argv[1]) == 0) {
+            printHelp();
             return 0;
         }
         
@@ -18,7 +29,7 @@ int main(int argc, char **argv) {
         runFile(vm, argv[1]);
         freeVM(vm);
     } else {
-        fprintf(stderr, "Usage: ilex [path]\n");
+        printHelp();
         exit(64);
     }
 
