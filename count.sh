0908844c8b78958796ce4c27826fb479a163d454
Skyler Burwell <skyboy49707@gmail.com>
2023-11-23 01:31:22 -0500

    Update scc scripts.

--- count.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/zsh
-cloc --exclude-dir=inc,cmake-build-debug,cmake-build-release,.idea,.vscode,lib,docs,tst --exclude-list-file=glad.c,CMakeLists.txt --exclude-ext=json,ts,d c/
--- scc.sh
+++ scc.sh
@@ -1,2 +1,2 @@
-@echo off
+#! /bin/zsh
 cloc . --by-file --exclude-dir=lib,inc,cmake-build-debug,cmake-build-release,.idea,.git,docs,benchmark,json,.vscode --not-match-f="CMakeLists.txt|scc|glad.c" --quiet --report-file=scc.txt
