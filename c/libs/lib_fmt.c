a570ec292d193042d17e38082c96ac8a0a9df1a3
Skyler Burwell <skyboy49707@gmail.com>
2025-03-06 18:46:53 -0700

    More terminal formatting with fmt.

--- c/libs/lib_fmt.c
+++ c/libs/lib_fmt.c
@@ -7,14 +7,33 @@
 #include <stdlib.h>
 
 static Value fmtPrintColors(VM *vm, const int argc, const Value *args) {
-    const int r = rand() % 2;
-    if (r == 0) {
-        for (int i = 0; i < 256; ++i) {
-            printf("\033[38;5;%dmThe quick brown fox jumped over the lazy dog. %d\n", i, i);
+    if (argc != 0 && argc != 1) {
+        runtimeError(vm, "Function printColors() expected 0 or 1 arguments but got '%d'.", argc);
+        return ERROR_VAL;
+    }
+
+    if (argc == 0) {
+        const int r = rand() % 2;
+        if (r == 0) {
+            for (int i = 0; i < 256; ++i) {
+                printf("\033[38;5;%dmThe quick brown fox jumped over the lazy dog. %d\n", i, i);
+            }
+        } else if (r == 1) {
+            for (int i = 0; i < 256; ++i) {
+                printf("\033[38;5;%dmBlack sphinx of quartz judge my vow. %d\n", i, i);
+            }
         }
-    } else if (r == 1) {
-        for (int i = 0; i < 256; ++i) {
-            printf("\033[38;5;%dmBlack sphinx of quartz judge my vow. %d\n", i, i);
+    } else {
+        if (!IS_STRING(args[0])) {
+            char *type = valueType(args[1]);
+            runtimeError(vm, "Function printColors() expected type 'string' for the first argument but got '%s'.", type);
+            free(type);
+            return ERROR_VAL;
+        }
+
+        const ObjString *str = AS_STRING(args[0]);
+        for (int i = 0; i < 255; ++i) {
+            printf("\033[38;5;%dm%s %d\n", i, str->str, i);
         }
     }
     printf("\033[0m");
@@ -23,14 +42,14 @@ static Value fmtPrintColors(VM *vm, const int argc, const Value *args) {
 }
 
 static Value fmtColor(VM *vm, const int argc, const Value *args) {
-    if (argc < 1 || argc > 3) {
-        runtimeError(vm, "Function color() expected 1 argument but got '%d'.", argc);
+    if (argc != 1) {
+        runtimeError(vm, "Function color8() expected 1 argument but got '%d'.", argc);
         return ERROR_VAL;
     }
 
     if (!IS_NUMBER(args[0])) {
         char *type = valueType(args[1]);
-        runtimeError(vm, "Function color() expected type 'number' for the first argument but got '%s'.", type);
+        runtimeError(vm, "Function color8() expected type 'number' for the first argument but got '%s'.", type);
         free(type);
         return ERROR_VAL;
     }
@@ -38,7 +57,45 @@ static Value fmtColor(VM *vm, const int argc, const Value *args) {
     const int c = AS_NUMBER(args[0]);
     printf("\033[38;5;%dm", c);
 
-    return OBJ_VAL(copyString(vm, "", 0));
+    char *fmt = (char*)malloc(sizeof(char) * 12 + 1);
+    sprintf(fmt, "\033[38;5;%dm", c);
+    return OBJ_VAL(takeString(vm, fmt, strlen(fmt)));
+}
+
+static Value fmtRgb(VM *vm, const int argc, const Value *args) {
+    if (argc != 3) {
+        runtimeError(vm, "Function rgb() expected 3 arguments but got '%d'.", argc);
+        return ERROR_VAL;
+    }
+
+    if (!IS_NUMBER(args[0])) {
+        char *type = valueType(args[1]);
+        runtimeError(vm, "Function rgb() expected type 'number' for the first argument but got '%s'.", type);
+        free(type);
+        return ERROR_VAL;
+    }
+
+    if (!IS_NUMBER(args[1])) {
+        char *type = valueType(args[1]);
+        runtimeError(vm, "Function rgb() expected type 'number' for the second argument but got '%s'.", type);
+        free(type);
+        return ERROR_VAL;
+    }
+
+    if (!IS_NUMBER(args[2])) {
+        char *type = valueType(args[1]);
+        runtimeError(vm, "Function rgb() expected type 'number' for the third argument but got '%s'.", type);
+        free(type);
+        return ERROR_VAL;
+    }
+
+    const int r = AS_NUMBER(args[0]);
+    const int g = AS_NUMBER(args[1]);
+    const int b = AS_NUMBER(args[2]);
+
+    char *fmt = (char*)malloc(sizeof(char) * 20 + 1);
+    sprintf(fmt, "\033[38;2;%d;%d;%dm", r, g, b);
+    return OBJ_VAL(takeString(vm, fmt, strlen(fmt)));
 }
 
 Value useFmtLib(VM *vm) {
@@ -52,7 +109,8 @@ Value useFmtLib(VM *vm) {
     }
 
     defineNative(vm, "printColors", fmtPrintColors, &lib->values);
-    defineNative(vm, "color", fmtColor, &lib->values);
+    defineNative(vm, "color8", fmtColor, &lib->values);
+    defineNative(vm, "rgb", fmtRgb, &lib->values);
 
     defineNativeValue(vm, "reset", OBJ_VAL(copyString(vm, "\033[0m", 4)), &lib->values);
     defineNativeValue(vm, "black", OBJ_VAL(copyString(vm, "\033[30m", 5)), &lib->values);
@@ -71,6 +129,28 @@ Value useFmtLib(VM *vm) {
     defineNativeValue(vm, "brightMagenta", OBJ_VAL(copyString(vm, "\033[95m", 5)), &lib->values);
     defineNativeValue(vm, "brightCyan", OBJ_VAL(copyString(vm, "\033[96m", 5)), &lib->values);
     defineNativeValue(vm, "white", OBJ_VAL(copyString(vm, "\033[97m", 5)), &lib->values);
+    defineNativeValue(vm, "resetColor", OBJ_VAL(copyString(vm, "\033[39m", 5)), &lib->values);
+    defineNativeValue(vm, "resetBgColor", OBJ_VAL(copyString(vm, "\033[49m", 5)), &lib->values);
+
+    defineNativeValue(vm, "bold", OBJ_VAL(copyString(vm, "\033[1m", 4)), &lib->values);
+    defineNativeValue(vm, "dim", OBJ_VAL(copyString(vm, "\033[2m", 4)), &lib->values);
+    defineNativeValue(vm, "italic", OBJ_VAL(copyString(vm, "\033[3m", 4)), &lib->values);
+    defineNativeValue(vm, "underline", OBJ_VAL(copyString(vm, "\033[4m", 4)), &lib->values);
+    defineNativeValue(vm, "doubleUnderline", OBJ_VAL(copyString(vm, "\033[21m", 5)), &lib->values); // Or reset bold, depends on terminal.
+    defineNativeValue(vm, "blink", OBJ_VAL(copyString(vm, "\033[5m", 4)), &lib->values);
+    defineNativeValue(vm, "fastBlink", OBJ_VAL(copyString(vm, "\033[6m", 4)), &lib->values);
+    defineNativeValue(vm, "inverted", OBJ_VAL(copyString(vm, "\033[7m", 4)), &lib->values);
+    defineNativeValue(vm, "hidden", OBJ_VAL(copyString(vm, "\033[8m", 4)), &lib->values);
+    defineNativeValue(vm, "strike", OBJ_VAL(copyString(vm, "\033[9m", 4)), &lib->values);
+
+    defineNativeValue(vm, "resetBold", OBJ_VAL(copyString(vm, "\033[21m", 5)), &lib->values); // Or double underline, depends on terminal.
+    defineNativeValue(vm, "resetIntensity", OBJ_VAL(copyString(vm, "\033[22m", 5)), &lib->values);
+    defineNativeValue(vm, "resetItalic", OBJ_VAL(copyString(vm, "\033[23m", 5)), &lib->values);
+    defineNativeValue(vm, "resetUnderline", OBJ_VAL(copyString(vm, "\033[24m", 5)), &lib->values);
+    defineNativeValue(vm, "resetBlink", OBJ_VAL(copyString(vm, "\033[25m", 5)), &lib->values);
+    defineNativeValue(vm, "resetInverted", OBJ_VAL(copyString(vm, "\033[27m", 5)), &lib->values);
+    defineNativeValue(vm, "resetHidden", OBJ_VAL(copyString(vm, "\033[28m", 5)), &lib->values);
+    defineNativeValue(vm, "resetStrike", OBJ_VAL(copyString(vm, "\033[29m", 5)), &lib->values);
 
 
     pop(vm);
